b5da0bf01894e20ee7248724a489d614894dd095
Dung T Pham <dtpham@linagora.com>
2018-11-28 11:18:59 +0700

    linshare-editor-onlyoffice-backend#7 refactor initialization order of webserver and mongo

--- src/webserver/app.js
+++ src/webserver/app.js
@@ -4,24 +4,34 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
+const config = require('config');
 const logger = require('../lib/logger');
 const api = require('./api');
 
 const app = express();
 let format = 'combined';
 
-app.server = http.createServer(app);
-if (process.env.NODE_ENV === 'development') {
-  format = 'dev';
+module.exports = {
+  init
+};
+
+function init() {
+  app.server = http.createServer(app);
+  if (process.env.NODE_ENV === 'development') {
+    format = 'dev';
+  }
+
+  app.use(morgan(format, { stream: logger.stream }));
+  app.use(cors({ origin: true, credentials: true }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  // TODO: add authorization method for getting static files
+  app.use('/files', express.static(path.join(__dirname, 'files')));
+  app.use('/api', api());
+
+  app.server.listen(process.env.PORT || config.webserver.port, () => {
+    logger.info(`Server started on port ${app.server.address().port}`);
+  });
 }
-
-app.use(morgan(format, { stream: logger.stream }));
-app.use(cors({ origin: true, credentials: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use('/files', express.static(path.join(__dirname, 'files')));
-app.use('/api', api());
-
-module.exports = app;
