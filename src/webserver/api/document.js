e8ac66f037913e2360eab28e1ed0f4d98752a46a
Dung T Pham <dtpham@linagora.com>
2018-11-30 16:01:01 +0700

    #11 check user authorization for incoming GET requests

--- src/webserver/api/document.js
+++ src/webserver/api/document.js
@@ -1,9 +1,9 @@
-const { validateSession, requireQueries } = require('../middleware');
+const { loadAuthorizedUser, requireQueries } = require('../middleware');
 const { getDocumentInfo } = require('../controller/document');
 
 module.exports = (router) => {
   router.get('/documents',
     requireQueries(['documentUuid', 'workGroupUuid']),
-    validateSession,
+    loadAuthorizedUser,
     getDocumentInfo);
 };
