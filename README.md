a19f2058b1e3b28ed32e182074e5496318731c56
Dung T Pham <dtpham@linagora.com>
2018-12-10 17:11:40 +0700

    Edit the script to save public key to LinShare

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
 # linshare-editor-onlyoffice
 
-## Project setup
+A backend application acts as a middleware between OnlyOffice document server and LinShare
+
+## Initial setup steps
+
+### Install dependencies
 ```
 npm install
 ```
 
-### Start server for development
+### Save public key to Linshare
+```
+LINSHARE_USER=root@localhost.localdomain LINSHARE_PASS=adminlinshare LINSHARE_HOST=http://localhost:30000 bash ./config/config_key.sh
+```
+> LINSHARE_USER and LINSHARE_PASS are Linshare administrator's username and password.
+
+## Development
+
+### Create a customized config file for development
+```
+cp ./config/default.json ./config/development.json
+```
+
+Some of notable configurations are:
+
+- **linshare.baseUrl**: base url to LinShare api.
+- **db.conectionString**: connection string to a MongoDB sever.
+
+
+### Start server
 ```
 npm start
 ```
@@ -14,12 +37,3 @@ npm start
 ```
 npm run test
 ```
-
-### Generate key pair and save public key to Linshare
-```
-LINSHARE_USER=root@localhost.localdomain LINSHARE_PASS=adminlinshare LINSHARE_HOST=http://localhost:30000 ISSUER=unique-issuer bash ./config/config_key.sh
-```
-
-> LINSHARE_USER and LINSHARE_PASS are Linshare admin username and password, this is needed in order to save public key to Linshare by sending a request to LINSHARE_HOST.
-
-> ISSUER is an arbitrary string and MUST be unique from existing issuers.
--- config/config_key.sh
+++ config/config_key.sh
@@ -6,8 +6,8 @@ SHELL_DIR="$( cd "$( dirname "$0" )" && pwd )"
 LINSHARE_USER=${LINSHARE_USER:-""}
 LINSHARE_PASS=${LINSHARE_PASS:-""}
 LINSHARE_HOST=${LINSHARE_HOST:-""}
-ISSUER=${ISSUER:-""}
-KEY_NAME=${KEY_NAME:-"jwtRS256.key"}
+ISSUER=${ISSUER:-"linshare-editor"}
+PUBLIC_KEY_FILE=${PUBLIC_KEY_FILE:-"jwt.key.pub"}
 
 cd "$SHELL_DIR" || exit 1
 
@@ -16,28 +16,17 @@ if [ ! "$LINSHARE_USER" ] || [ ! "$LINSHARE_PASS" ] || [ ! "$LINSHARE_HOST" ]; t
     exit 1
 fi
 
-if ! type ssh-keygen >/dev/null 2>&1; then
-    echo "E: Require ssh-keygen command!"
-    exit 1
-fi
-if ! type openssl >/dev/null 2>&1; then
-    echo "E: Require openssl command!"
-    exit 1
-fi
-
 if ! type curl >/dev/null 2>&1; then
     echo "E: Require curl command!"
     exit 1
 fi
 
-if [ -e "${KEY_NAME}" ]; then
-    rm -f "${KEY_NAME}" "${KEY_NAME}.pub"
+if [ ! -e "${PUBLIC_KEY_FILE}" ]; then
+    echo "E: Missing public key file ${PUBLIC_KEY_FILE}"
+    exit 1
 fi
 
-ssh-keygen -t rsa -b 4096 -f "${SHELL_DIR}/${KEY_NAME}" -N ""
-openssl rsa -in "${SHELL_DIR}/${KEY_NAME}" -pubout -outform PEM -out "${SHELL_DIR}/${KEY_NAME}.pub"
-
-PUBLIC_KEY_ONELINE=$(sed ':a;N;$!ba;s/\n/\\n/g' "${KEY_NAME}.pub")
+PUBLIC_KEY_ONELINE=$(sed ':a;N;$!ba;s/\n/\\n/g' "${PUBLIC_KEY_FILE}")
 if ! curl -H 'Accept: application/json' -H 'Content-Type: application/json' "${LINSHARE_HOST}/linshare/webservice/rest/admin/public_keys" -u "${LINSHARE_USER}:${LINSHARE_PASS}" \
 -d '{
   "domainUuid" : "LinShareRootDomain",
@@ -46,7 +35,6 @@ if ! curl -H 'Accept: application/json' -H 'Content-Type: application/json' "${L
   "format" : "PEM"
 }' -i 2>/dev/null| head -n1 | grep 200 >/dev/null 2>&1; then
     echo "Error, make sure your ENV variables are correct and issuer is not duplicated"
-    rm -f "${KEY_NAME}" "${KEY_NAME}.pub"
     exit 1
 fi
 echo "Success"
