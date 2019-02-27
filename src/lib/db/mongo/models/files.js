6a3f1ce0fca8171b8cf0f495f2ffdf014aff1659
Dang Dien PHAN <ddphan@linagora.com>
2019-02-27 15:11:30 +0700

    linagora/lgs/linshare/products/linshare-editor-onlyoffice-backend#26 - remove unnecessary fields and add key (onlyoffice key)

--- src/lib/db/mongo/models/files.js
+++ src/lib/db/mongo/models/files.js
@@ -2,17 +2,8 @@ const mongoose = require('mongoose');
 
 const FilesSchema = new mongoose.Schema({
   uuid: { type: String, unique: true, required: true },
-  workGroup: { type: String, required: true },
-  parent: { type: String },
-  path: { type: String },
-  name: { type: String },
-  mimeType: { type: String },
-  state: { type: String, required: true },
-  size: { type: Number },
-  sha256sum: { type: String },
-  description: { type: String },
-  fileType: { type: String },
-  documentType: { type: String }
+  key: { type: String, unique: true, required: true },
+  state: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('files', FilesSchema);
