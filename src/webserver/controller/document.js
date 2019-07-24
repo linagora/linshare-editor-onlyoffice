dd12799460150ce4aef511e768c0af0298528f7e
Tuan Tuan LE <tlcong@linagora.com>
2019-07-24 18:02:37 +0700

    #34 move explaination comment to the proper method

--- src/webserver/controller/document.js
+++ src/webserver/controller/document.js
@@ -64,27 +64,6 @@ async function update(req, res) {
       await document.update(url);
       await document.remove(); // Only remove local file and generate new key when the document is normal saved (not force save)
 
-      /*
-      Currently, the key sent to document server will
-      immediately become invalid after document server
-      sends a request to callbackUrl along with the status
-      of 2 and the edited document download url.
-
-      To avoid users try to access the document using
-      the old invalid key while it being saved to Linshare,
-      let the user wait until saving is done and new key
-      is generated (by set "saving" state for the document).
-
-      This solution does not totally get rid of invalid key
-      problem as the user might try to access the document
-      by the time document state being updated from "downloaded"
-      to "saving". Having said that, this will minimize the
-      possibility.
-
-      Read more:
-      https://api.onlyoffice.com/editors/troubleshooting#key
-      https://github.com/ONLYOFFICE/DocumentServer/issues/513
-      */
       pubsub.topic(PUBSUB_EVENTS.DOCUMENT_SAVED).publish(document); // Successfully saving file to Linshare
     }
   } catch (error) {
